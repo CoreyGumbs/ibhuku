35f4b311a8b2c6adf3e18fe0af6fecce56b0d220
Corey Gumbs <coreygumbs@gmail.com>
2017-02-20 19:07:52 -0500

    [chore] Added AUTH_USER_MODEL to settings

--- users/models.py
+++ users/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+	first_name = models.CharField(max_length=100, blank=False)
+	last_name = models.CharField(max_length=100, blank = True)
