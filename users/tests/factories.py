e995d557683a7626d4c98a9edd252abe4096a387
Corey Gumbs <coreygumbs@gmail.com>
2017-02-20 23:23:45 -0500

    [chore] Added RandomUserFactory fixture, updated model tests

--- users/tests/factories.py
+++ users/tests/factories.py
@@ -10,8 +10,19 @@ class UserFactory(factory.django.DjangoModelFactory):
 	class Meta:
 		model = User
 
+	first_name = 'Testy'
+	last_name = 'McTesty'
+	username = factory.LazyAttribute(lambda n: '{0}{1}'.format(n.first_name, n.last_name[:3]))
+	email = factory.LazyAttribute(lambda n: '{0}@testing.com'.format(n.first_name))
+	password = 'testpassword'
+
+
+class RandomUserFactory(factory.django.DjangoModelFactory):
+	class Meta:
+		model = User
+
 	first_name = factory.Faker('first_name', locale='en_US')
 	last_name = factory.Faker('last_name', locale='en_US')
 	username = factory.LazyAttribute(lambda n: '{0}{1}'.format(n.first_name, n.last_name[:3]))
 	email = factory.LazyAttribute(lambda n: '{0}@testing.com'.format(n.first_name))
-	password = make_password('testpassword')
+	password = 'testpassword'
--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -2,19 +2,30 @@
 import pytest
 
 from users.models import User
-from users.tests.factories import UserFactory
+from users.tests.factories import UserFactory, RandomUserFactory
 
 @pytest.mark.django_db
 class TestIUserModel:
 
     def setup(self):
-        self.ran_user = UserFactory()
-        self.user1 =  UserFactory(first_name='Testy')
+       self.user1 = UserFactory()
+       self.user2 = UserFactory(first_name='John', last_name='', email='DoeBoy123@testing.com', )
+       self.users = User.objects.all()
 
-    def test_user_instance(self):
-        users = User.objects.all()
+    def test_user_instance_created(self):
         assert self.user1.first_name == 'Testy'
-        assert len(users) == 2
+        assert self.user1.email == 'Testy@testing.com'
+        assert self.user1.password == 'testpassword'
+
+    def test_multiple_users_created(self):
+        assert len(self.users) == 2
+
+    def test_user_instance_get_full_name_method(self):
+        assert self.user1.get_full_name() == 'TMcTesty'
+
+    def test_user_no_last_name_get_full_name_method(self):
+        assert self.user2.get_full_name() == 'John'
+       
 
 
 
