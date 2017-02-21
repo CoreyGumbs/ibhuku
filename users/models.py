0f13b8f60b70d06160f81eecff7e202a9fa387ba
Corey Gumbs <coreygumbs@gmail.com>
2017-02-20 21:37:35 -0500

    [chore] Added Model Field Updates, Model Manager, Updated Test JSON fixture

--- users/models.py
+++ users/models.py
@@ -2,8 +2,27 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+
+from users.managers import UserManager
 
 # Create your models here.
 class User(AbstractBaseUser, PermissionsMixin):
-	first_name = models.CharField(max_length=100, blank=False)
-	last_name = models.CharField(max_length=100, blank = True)
+	first_name = models.CharField(_('first name'), max_length=100, blank=False)
+	last_name = models.CharField(_('last name'),max_length=100, blank = True)
+	username = models.CharField(_('username'), max_length=50, unique=True, blank=True)
+	email =  models.EmailField(_('email'),max_length=255, unique=True)
+	date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+	last_login = models.DateTimeField(_('last login'), auto_now=True)
+	is_active =  models.BooleanField(_('active'), default=True)
+	is_staff = models.BooleanField(_('staff'), default=False)
+
+	objects = UserManager()
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['first_name']
+
+	class Meta:
+		db_table = 'users'
+		verbose_name = _('user')
+		verbose_name_plural = _('users') 
--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 import pytest
 
+from users.models import User
+
+@pytest.mark.django_db
+class TestIUserModel:
+    pass
+
+
+
+
 
