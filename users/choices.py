ed1047a2973c23b3f1319297bb78ee76a2790d99
Corey Gumbs <coreygumbs@gmail.com>
2017-02-23 19:50:32 -0500

    [fix] removed profile choices from user choices.py

--- users/choices.py
+++ users/choices.py
@@ -6,20 +6,3 @@ ACCOUNT_TYPE = (
         ('BIZ', _('Business')),
         ('EDU', _('Education')),
     )
-
-GENDER_CHOICES = (
-        ('N', _('N/A')),
-        ('M', _('Male')),
-        ('F', _('Female')),
-        ('O', _('Other')),
-
-    )
-
-EDUCATION_CHOICES = (
-        ('NA', _('N/A')),
-        ('HS', _('Diploma')),
-        ('AD', _('Associates')),
-        ('BD', _('Bachelors')),
-        ('MD', _('Masters')),
-        ('PH', _('Doctorate')),
-    )
