ee7b6a07e9fb6d7f7372bd1bab209f91e1ae986d
Corey Gumbs <coreygumbs@gmail.com>
2017-02-24 01:54:53 -0500

    [chore] Added is_form test to UserRegistrationForm test.

--- users/tests/test_forms/test_user_registration_form.py
+++ users/tests/test_forms/test_user_registration_form.py
@@ -18,21 +18,37 @@ class TestUserRegisrationForm:
         Sets up test fixtures using Factory Boy instances. See factories.py module
         for more information.
         """
+        self.form = UserRegistrationForm()
         self.user = UserFactory()
 
     def test_user_registration_form_is_not_bound(self):
         """
         Test if form is bound by passed data.
         """
-        form = UserRegistrationForm()
-        assert form.is_bound == False, 'Returns False if form is not bound by data.(no values passed)'
+        assert self.form.is_bound == False, 'Returns False if form is not bound by data.(no values passed)'
 
     def test_user_registration_form_is_bound(self):
         """
         Test if form is bound by passed data.
         """
-        form = UserRegistrationForm(data={})
+        self.form = UserRegistrationForm(data={})
 
         #As per Django documentation, the data={} will return True even if no kwargs passed.
-        assert form.is_bound == True, 'Returns True if form is bound by data.'
+        assert self.form.is_bound == True, 'Returns True if form is bound by data.'
+
+    def test_user_registration_form_is_valid(self):
+
+        #Hardcoded kwargs as form wont take self.data fixture. Will return False.
+        self.form2 = UserRegistrationForm(data={
+                'first_name': 'Testy',
+                'last_name': 'McTesty',
+                'email': 'McTesty@testing.com',
+                'password': 'testpassword1234',
+                'acct_type': 'IND',
+                'toc': False,
+            })
+
+        assert self.form.is_valid() ==  False
+        assert self.form2.is_valid() == True
+
 
