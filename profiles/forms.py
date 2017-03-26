18d86d2d50fb1dbc484d6e3a403799a6cfd09d86
Corey Gumbs <coreygumbs@gmail.com>
2017-03-26 01:32:20 -0400

    [fix] added pytest mark db decorator to profiles forms test

--- profiles/forms.py
+++ profiles/forms.py
@@ -6,3 +6,19 @@ from django.forms import ModelForm
 
 from profiles.models import Profile
 from users.models import User
+
+
+class ProfileUpdateForm(ModelForm):
+    bio = forms.CharField()
+
+    class Meta:
+        model = Profile
+        fields = ('bio', 'location',)
+        widgets = {
+            'bio': forms.Textarea(attrs={
+                'rows': 10,
+                'columns': 20,
+                'style': 'resize:none;',
+                'max_length': 150
+            })
+        }
--- profiles/tests/test_forms/test_update_profile_form.py
+++ profiles/tests/test_forms/test_update_profile_form.py
@@ -6,9 +6,11 @@ import factory
 import factory.django
 
 from profiles.models import Profile
+from profiles.forms import ProfileUpdateForm
 from profiles.tests.factories import UserFactory
 
 
+@pytest.mark.django_db
 class TestUpdateProfileForm:
     """
     Test UpdateProfileForm
@@ -20,3 +22,18 @@ class TestUpdateProfileForm:
         """
         self.user = UserFactory()
         self.profile = Profile.objects.get(user_id=self.user.id)
+        self.form = ProfileUpdateForm()
+
+    def test_profile_update_form_is_not_bound(self):
+        """
+        Test ProfileUpdateForm is bound.
+        """
+        assert self.form.is_bound == False, 'Returns False if form is not bound.'
+
+    def test_profile_update_form_is_bound(self):
+        """
+        Test ProfileUpdateForm is bound
+        """
+        form = ProfileUpdateForm(data={})
+
+        assert form.is_bound == True, 'Returns True if form is bound.'
