a25d14a7afdcba12a18753c2eb0b965b461c0c75
Corey Gumbs <coreygumbs@gmail.com>
2017-04-10 16:06:25 -0400

    [chore] Added Tempfile for image file and tests.

--- profiles/tests/test_forms/test_avatar_upload_form.py
+++ profiles/tests/test_forms/test_avatar_upload_form.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-
+import tempfile
 import pytest
 import factory
 import factory.django
@@ -23,18 +23,34 @@ class TestAvatarUploadForm:
         """
         self.user = UserFactory()
         self.profile = Profile.objects.get(user_id=self.user.id)
+        images = {
+            'jpg': tempfile.NamedTemporaryFile(suffix='.jpg').name,
+            'png': tempfile.NamedTemporaryFile(suffix='.png').name,
+            'gif': tempfile.NamedTemporaryFile(suffix='.gif').name,
+        }
+
         ProfileAvatar.objects.create(
-            profile_id=self.profile.id, avatar='name.jpg')
+            profile_id=self.profile.id, avatar=images['jpg'])
+
         self.avatar = ProfileAvatar.objects.select_related(
             'profile').get(profile_id=self.profile.id)
-        self.form = AvatarUploadForm({})
+
+        self.form = AvatarUploadForm(
+            data={'avatar': images['jpg']})
 
     def test_avatar_update_is_bound_method(self):
+        """
+        Test form's is_bound() method.
+        """
         assert self.form.is_bound == True, 'Should return True if form is bound.'
         form = AvatarUploadForm()
         assert form.is_bound == False, 'Should return False if form is not bound.'
 
     def test_avatar_update_is_valid_method(self):
-        assert self.form.is_valid == True, 'Should return True if form is valid. Check form data kwargs.'
-        form = AvatarUploadForm({})
-        assert form.is_valid == False, 'Should return False if form is not valid.'
+        """
+        Test form's is_valid() method.
+        """
+        assert self.form.is_valid(
+        ) == True, 'Should return True if form is valid. Check form data kwargs.'
+        form = AvatarUploadForm()
+        assert form.is_valid() == False, 'Should return False if form is not valid.'
