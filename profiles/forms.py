248f3fac1c8c943b61f2ffe20c092c13b433ecb1
Corey Gumbs <coreygumbs@gmail.com>
2017-05-18 19:29:31 -0400

    [chore] Added profile social media form values and error tests.

--- profiles/forms.py
+++ profiles/forms.py
@@ -163,4 +163,5 @@ class ProfileSocialMediaForm(ModelForm):
 
     class Meta:
         model = ProfileSocial
-        fields = ['facebook']
+        fields = ['facebook', 'twitter', 'google', 'instagram',
+                  'linkedin', 'pintrest', 'website']
--- profiles/tests/test_forms/test_social_links_form.py
+++ profiles/tests/test_forms/test_social_links_form.py
@@ -8,7 +8,7 @@ import factory.django
 from django.test import override_settings
 
 from users.models import User
-from profiles.models import Profile, ProfileAvatar
+from profiles.models import Profile, ProfileAvatar, ProfileSocial
 from profiles.forms import ProfileSocialMediaForm
 from profiles.tests.factories import UserFactory
 
@@ -25,12 +25,15 @@ class TestSocialLinksForm:
         """
         self.user = UserFactory()
         self.profile = Profile.objects.get(user_id=self.user.id)
-        self.avatar = ProfileAvatar.objects.select_related(
+        self.social_media = ProfileSocial.objects.select_related(
             'profile').get(profile_id=self.profile.id)
         self.data = {
             'facebook': 'http://www.facebook.com',
             'twitter': 'http://www.twitter.com',
             'google': 'http://www.google.com',
+            'instagram': 'http://www.instagram.com',
+            'pintrest': 'http://www.pintrest.com',
+            'website': 'http://www.coreygumbs.com',
         }
 
     def test_social_media_form_is_bound_method(self, client):
@@ -46,4 +49,27 @@ class TestSocialLinksForm:
         """
         Test form data is valid.
         """
-        pass
+        valid_form = ProfileSocialMediaForm(self.data)
+        assert valid_form.is_valid() == True, "Returns True if form data valid."
+
+    def test_social_media_form_values(self, client):
+        """
+        Test form passes data.
+        """
+        form = ProfileSocialMediaForm(
+            data=self.data)
+        assert form['facebook'].value(
+        ) == 'http://www.facebook.com', 'Returns value of data passed to form.'
+        assert form['website'].value(
+        ) == 'http://www.coreygumbs.com', 'Returns value of data passed to form.'
+
+    def test_social_media_form_errors(self, client):
+        """
+        Test form errors.
+        """
+        form = ProfileSocialMediaForm(
+            data={'facebook': 'facebook'})
+        assert form.has_error(
+            'facebook') == True, 'Should return True if there is an error'
+        assert form.errors == {'facebook': [
+            'Enter a valid URL.']}, 'Should return field error message'
